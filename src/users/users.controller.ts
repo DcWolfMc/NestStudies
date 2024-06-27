c05edcb3382d87c2026327d1fd1905ec0689ab51
danielmcolares <danielmcolares@unifor.br>
2024-06-27 17:17:21 -0300

    learning controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?:'INTERN' | "ADMIN"|'ENGINEER') {
+    return [];
+  }
+
+  @Get('interns') // GET /users/interns
+  findAllInterns() {
+    return [];
+  }
+
+  //:id most be declared after all possible users get calls
+  //its a waterfall
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string,@Body() userUpdate:{}) {
+    return { id , ...userUpdate};
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+
+}
