d32248fdf01c75169119864cd90324e28568042e
danielmcolares <danielmcolares@unifor.br>
2024-07-05 15:19:43 -0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api')
+
+  // About cores, study more to better config it.
+  app.enableCors()
+
   await app.listen(3000);
 }
 bootstrap();
