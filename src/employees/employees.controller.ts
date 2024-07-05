d5267db1b1a55b04d5f70172463dc4035a464ea8
danielmcolares <danielmcolares@unifor.br>
2024-07-05 15:33:54 -0300

    Rate limiting with Nest Throttler

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -10,7 +10,11 @@ import {
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
+// To skip throttle to the full controller, just place the SkipThrottle here,
+// at top of controller.
+SkipThrottle();
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
@@ -20,11 +24,15 @@ export class EmployeesController {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  // To apply the throttle to a especific route, use the comand below.
+  @SkipThrottle({ default: false })
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
     return this.employeesService.findAll(role);
   }
-
+  
+  // to reconfigure the throttle, use Throttle at top of the route.
+  @Throttle({ short: { ttl: 1000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
