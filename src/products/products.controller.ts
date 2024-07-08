79094ff3470c37ca5851d52dde0fd6eb0b4bc301
danielmcolares <danielmcolares@unifor.br>
2024-07-08 17:01:38 -0300

    ajusting Products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,40 @@
-import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
+  @Post()
+  creat() {}
+  @Post()
+  createGroup() {}
+  @Get()
+  getProducts(
+    @Query('category') category: string[],
+    @Query('rating', ParseIntPipe) rating: number[],
+    @Query('brands') brands: string[],
+    @Query('minPrice', ParseIntPipe) minPrice: number,
+    @Query('maxPrice', ParseIntPipe) maxPrice: number,
+    @Query('sortBy') sortBy: "price"|"name",
+  ) {}
 
-    @Get()
-    getProducts(@Query('category') category:string){}
-
-    @Get(":id")
-    findOne(@Param('id', ParseIntPipe) id: number){
-        return {}
-    }
-
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return {};
+  }
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number) {
+    return {};
+  }
+  //Would it be interesting to update a group of items instead of just 1?
+  @Patch(':id')
+  updateGroup(@Param('id', ParseIntPipe) id: number) {
+    return {};
+  }
 }
