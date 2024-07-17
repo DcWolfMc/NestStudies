ec045577f9229eb96a149b89893e3ec4c994f7db
Daniel <danielmcolares@gmail.com>
2024-07-17 15:58:27 -0300

    fix: removing logs

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -17,13 +17,10 @@ export class CategoriesService {
   async find(query: GetCategoryDto) {
     const { categories, products } = query;
     let categoryIds = []
-    console.log("products",typeof products,products);
     
     if(categories)
       categoryIds = categories.split(',').map((id) => parseInt(id, 10));
     // const whereClause = categoryIds.length > 0 ? { id: { in: categoryIds } } : {};
-    console.log("categoryIds",categoryIds);
-    console.log("categoryIds.length < 0",categoryIds.length < 0, categoryIds.length,!categoryIds.length);
     
     if (!categoryIds.length) {
       return await this.databaseService.category.findMany({
